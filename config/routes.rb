fb8fa803a931bfd9da637cf310a6885c5bbb1044
Ken Collins <ken@metaskills.net>
2020-04-06 20:14:07 -0400

    Add some simple smoke tests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   root to: 'application#index'
   resources :docs, only: [:show]
+  resources :smokes, only: [:index] do
+    collection do
+      post   'login'
+      delete 'logout'
+      get    'cache'
+      get    'exception'
+      get    'image'
+    end
+  end
 end
