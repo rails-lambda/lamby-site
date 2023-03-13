551b6505743196272afc2a81705de820d4b30b7b
Ken Collins <ken@metaskills.net>
2023-03-13 08:37:08 -0400

    Quick Start Security Callout.

--- src/components/DocLink.js
+++ src/components/DocLink.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const DocNames = {
-  "anatomy": "How Lamby Works",
-  "cpu": "CPU Architecture",
-  "environment": 'ENV Variables & Secrets',
-  "assets": 'JavaScript & Assets',
-  "deploy": 'Build & Deploy',
-  "custom-domain": 'Custom Domain Names',
-  "activejob": 'ActiveJob & Background Processing',
-  "observability": 'Logging & Observability',
-  "database": 'Database & VPCs',
-  "webservers": 'Web Proxy Integrations'
-}
+  anatomy: "How Lamby Works",
+  cpu: "CPU Architecture",
+  environment: "ENV Variables & Secrets",
+  assets: "JavaScript & Assets",
+  deploy: "Build & Deploy",
+  "custom-domain": "Custom Domain Names",
+  activejob: "ActiveJob & Background Processing",
+  observability: "Logging & Observability",
+  database: "Database & VPCs",
+  webservers: "Web Proxy Integrations",
+};
 
-const titleize = function(str) {
-  return str.replace(/(_|-)/g, ' ').split(' ').map(w => w.charAt(0).toUpperCase() + w.toLowerCase().slice(1)).join(' ')
-}
+const titleize = function (str) {
+  return str
+    .replace(/(_|-)/g, " ")
+    .split(" ")
+    .map((w) => w.charAt(0).toUpperCase() + w.toLowerCase().slice(1))
+    .join(" ");
+};
 
-export default function DocLink({id, name}) {
+export default function DocLink({ id, name, anchor }) {
   const aLink = name || DocNames[id] || titleize(id);
-  return (
-    <a href={`/docs/${id}`}>{aLink}</a>
-  );
+  if (anchor) {
+    return <a href={`/docs/${id}#${anchor}`}>{aLink}</a>;
+  } else {
+    return <a href={`/docs/${id}`}>{aLink}</a>;
+  }
 }
